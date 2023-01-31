72556aa4b3e11c9c714b35b2464413c6da98b37d
maks105fmm <maks105fmm@gmail.com>
2023-01-31 21:39:08 +0300

    Task2 - 1.1.3 Build

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorld - " + (bean == bean1));
+        System.out.println("Cat - " + (cat == cat1));
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -10,5 +10,13 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+
+    }
+        @Bean(name="cat")
+        @Scope("prototype")
+        public Cat getCat() {
+            Cat cat = new Cat();
+            cat.setMessage("cat");
+            return cat;
     }
 }
